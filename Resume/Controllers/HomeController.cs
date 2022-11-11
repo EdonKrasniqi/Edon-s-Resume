6e92fd6d22212012f6a5a6c2be8953caa7dc5dbf
Edon Krasniqi <ek48587@ubt-uni.net>
2022-11-11 15:38:29 +0100

    Created Navbar

---
+++ Resume/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Resume.Models;
+using System.Diagnostics;
+
+namespace Resume.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        //public async Task<object> Post([FromBody] Class contact)
+        //{
+        //    try
+        //    {
+        //        Class model = await _productRepositry.CreateUpdateProduct(productDto);
+        //        _response.Result = model;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _response.IsSuccess = false;
+        //        _response.ErrorMessages = new List<string>() { ex.ToString() };
+        //    }
+        //    return _response;
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
