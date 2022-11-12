f0c6611507d9b1bcec76e4e3f793be317daf9552
Edon Krasniqi <ek48587@ubt-uni.net>
2022-11-12 18:12:17 +0100

    Active-dot and Scroll to top functions

--- Resume/wwwroot/js/site.js
+++ Resume/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var header = document.getElementById("active-dot");
+var btns = header.getElementsByClassName("nav-dot");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
